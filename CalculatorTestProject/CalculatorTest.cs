c76a1da64c8ba32eb664e3c1298c6874da77b402
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-25 14:57:56 +0200

    aggiornamento commenti

--- CalculatorTestProject/CalculatorTest.cs
+++ CalculatorTestProject/CalculatorTest.cs
@@ -10,42 +10,49 @@ namespace CalculatorTestProject
         }
 
         [Test]
+        // Test funzione Add del Calculator
         public void AddTest()
         {
             Assert.That(Calculator.Add(2, 3), Is.EqualTo(5));
         }
 
         [Test]
+        // Test funzione Subtract del Calculator
         public void SubtractTest()
         {
             Assert.That(Calculator.Subtract(5, 2), Is.EqualTo(3));
         }
 
         [Test]
+        // Test funzione Subtract del Calculator con numeri invertiti
         public void InvertSubtractTest()
         {
             Assert.That(Calculator.Subtract(2, 5), Is.EqualTo(3));
         }
 
         [Test]
+        // Test funzione Divide del Calculator
         public void DivideTest()
         {
             Assert.That(Calculator.Divide(6, 2), Is.EqualTo(3));
         }
 
         [Test]
+        // Test funzione Divide del Calculator con uno dei parametri a 0
         public void DivideZeroTest()
         {
             Assert.That(Calculator.Divide(6, 0), Is.EqualTo(0));
         }
 
         [Test]
+        // Test funzione Multiply del Calculator
         public void MultiplyTest()
         {
             Assert.That(Calculator.Multiply(3, 2), Is.EqualTo(6));
         }
 
         [Test]
+        // Test funzione Multiply del Calculator con uno dei parametri a 0
         public void MultiplyZeroTest()
         {
             Assert.That(Calculator.Multiply(0, 3), Is.EqualTo(0));
