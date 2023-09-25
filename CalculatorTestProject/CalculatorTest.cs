94147a18ff64fac2e48e4cbeaf6ae74345fe5275
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-25 14:07:14 +0200

    Creazione NUnit Test project, con classe Calculator Test

---
+++ CalculatorTestProject/CalculatorTest.cs
@@ -0,0 +1,16 @@
+namespace CalculatorTestProject
+{
+    public class CalculatorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
